a42bc139d52c2d44cdd8247d3aa5e9c168038452
varalamanichandrasai223-wq <varalamanichandrasai223@gmail.com>
2026-08-19 15:46:22 +0530

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,16 @@
+print("For Loop:")
+for i in range(1, 6):
+    print(i)
+print("\nLoop through a list:")
+fruits = ["Apple", "Banana", "Mango", "Orange"]
+for fruit in fruits:
+    print(fruit)
+print("\nWhile Loop:")
+count = 1
+while count <= 5:
+    print(count)
+    count += 1
+print("\nEven numbers:")
+for number in range(1, 11):
+    if number % 2 == 0:
+        print(number)
